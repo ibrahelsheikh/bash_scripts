de4b01968713ef29ddc9fe3ed517ec558e3bed15
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-10-07 22:30:45 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,23 +1,23 @@
-# Replace 'package_name' with the name of the package you want to check
-if dpkg -l | grep -q rsync; then
-    echo "Package is installed."
-else
-    echo "Package is not installed."
-    echo "Installing package..."
-
-    sudo apt update
-    sudo apt install rsync
-
-fi
-
-# rsync files from source to destination
 #!/bin/bash
 
-source_dir="/mnt/c/HDD/Data Tanta uinversity Lectures & Sections"
-destination_dir="/mnt/d/Data Tanta uinversity Lectures & Sections"
+# Check if rsync is installed
+if dpkg -l | grep -q rsync; then
+    echo "Rsync package is installed."
+else
+    echo "Rsync package is not installed."
+    echo "Installing rsync package..."
+    
+    # Update package repository and install rsync
+    sudo apt update
+    sudo apt install rsync
+fi
 
-# Copy missing files from source to destination
+# Define source and destination directories
+source_dir="/mnt/c/HDD/Data Tanta University Lectures & Sections"
+destination_dir="/mnt/d/Data Tanta University Lectures & Sections"
+
+# Sync missing files from source to destination
 rsync -av --ignore-existing "$source_dir/" "$destination_dir/"
 
-# Copy missing files from destination to source
+# Sync missing files from destination to source
 rsync -av --ignore-existing "$destination_dir/" "$source_dir/"
