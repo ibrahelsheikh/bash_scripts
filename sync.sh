c532a885bd73c27a76c6e4ad38b02ae76483b68a
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-11-16 17:39:05 +0200

    Added defragment to hdd

--- sync.sh
+++ sync.sh
@@ -14,10 +14,15 @@ fi
 
 # Define source and destination directories
 source_dir="/mnt/c/HDD/Data Tanta University Lectures & Sections"
-destination_dir="/mnt/d/Data Tanta University Lectures & Sections"
+destination_dir="/mnt/d/Data Tanta University Lectures & Sections" 
+
+/mnt/c/Windows/System32/defrag.exe D: /U /V
+
 
 # Sync missing files from source to destination
 rsync -av --ignore-existing "$source_dir/" "$destination_dir/"
 
 # Sync missing files from destination to source
 rsync -av --ignore-existing "$destination_dir/" "$source_dir/"
+
+/mnt/c/Windows/System32/defrag.exe D: /U /V
